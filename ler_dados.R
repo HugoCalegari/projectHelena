b5da604a02c36bafcff27f386fab32a8ba15eb17
HugoCalegari <hugo16calegari@gmail.com>
2017-09-05 10:33:08 -0300

    Adição de coisas novas nos códigos.

--- ler_dados.R
+++ ler_dados.R
@@ -1,10 +1,10 @@
 library(readxl)
 library(dplyr)
 library(ggplot2)
+library(plyr)
 setwd("~/Documentos/ME810/HELENA/projectHelena/")
 
-source("funcoes.R")
-
+###################################################################### 1ª forma
 controle <- as.data.frame(read_excel("./dados/DADOS COMPRIMENTO NANO UNICAMP.xlsx", sheet = "controle"))
 t1 <- as.data.frame(read_excel("./dados/DADOS COMPRIMENTO NANO UNICAMP.xlsx", sheet = "t0.006"))
 t2 <- as.data.frame(read_excel("./dados/DADOS COMPRIMENTO NANO UNICAMP.xlsx", sheet = "t0.0125"))
@@ -12,7 +12,7 @@ t3 <- as.data.frame(read_excel("./dados/DADOS COMPRIMENTO NANO UNICAMP.xlsx", sh
 t4 <- as.data.frame(read_excel("./dados/DADOS COMPRIMENTO NANO UNICAMP.xlsx", sheet = "t0.05"))
 t5 <- as.data.frame(read_excel("./dados/DADOS COMPRIMENTO NANO UNICAMP.xlsx", sheet = "t0.1"))
 
-#Substitui os caracteres "Morta" por "NA".
+#Substitui os caracteres "Morta" por "NA". Estes comando é para o caso dos dados não organizados como em data_compact
 controle$`8` <- gsub("Morta", "NA", controle$`8`)
 t2[3,] <- gsub("Morta", "NA", t2[3,])
 t3[9,] <- gsub("Morta", "NA", t3[9,])
@@ -35,16 +35,20 @@ dados[[4]]$`8` <- as.numeric(dados[[4]]$`8`)
 dados[[6]]$`7` <- as.numeric(dados[[6]]$`7`)
 dados[[6]]$`8` <- as.numeric(dados[[6]]$`8`)
 
-#Obter um vetor de médias para cada dia, em controle e tratamentos
-media_controle <- sapply(na.omit(dados[[1]][,2:10]), mean)
-media_t1 <- sapply(na.omit(dados[[2]][,2:10]), mean)
-media_t2 <- sapply(na.omit(dados[[3]][,2:10]), mean)
-media_t3 <- sapply(na.omit(dados[[4]][,2:10]), mean)
-media_t4 <- sapply(na.omit(dados[[5]][,2:10]), mean)
-media_t5 <- sapply(na.omit(dados[[6]][,2:10]), mean)
+###################################################################### 2º forma
+# 0 = controle, 1 = t0.006, 2 = t0.0125, 3 = t0.025, 4 = t0.05 e 5 = t0.1
+data_compact <- as.data.frame(read_excel("./dados/nanoparticulas_dadosR.xlsx"))
+data_compact$Tratamento <- as.factor(data_compact$Tratamento)
+data_compact$Comprimento <- as.numeric(data_compact$Comprimento)
+data_compact$Dia <- as.factor(data_compact$Dia)
+data_compact$Repeticao <- as.factor(data_compact$Repeticao)
 
-medias <- list(media_controle, media_t1, media_t2, media_t3, media_t4, media_t5)
-c(media_controle, media_t1, media_t2, media_t3, media_t4, media_t5)
+prop.table(table(is.na(data_compact))) #quantidade de NA's geral
 
-#Obter um veto de variância para cada indivíduo
-var_controle <- sapply(na.omit(dados[[1]][1:10,2:10]), var)
+#
+teste <- as.factor(c(rep(1:9, 60)))
+data_compact <- cbind(teste, data_compact)
+
+ggplot(data_compact, aes(Dia, Comprimento, color = Tratamento)) + geom_boxplot(outlier.colour = "red", outlier.shape = 1)
+
+#
