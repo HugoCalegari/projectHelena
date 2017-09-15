4ae6ab5f7ec502e6c6c74063864aa47b8bb63f39
HugoCalegari <hugo16calegari@gmail.com>
2017-09-15 12:43:41 -0300

    Adição de código para gráfico de perfil médio para comprimento para nanopartículas. Esta em ler_dados.

--- ler_dados.R
+++ ler_dados.R
@@ -2,6 +2,7 @@ library(readxl)
 library(dplyr)
 library(ggplot2)
 library(plyr)
+library(plotrix)
 setwd("~/Documentos/ME810/HELENA/projectHelena/")
 
 ###################################################################### 1ª forma
@@ -37,18 +38,65 @@ dados[[6]]$`8` <- as.numeric(dados[[6]]$`8`)
 
 ###################################################################### 2º forma
 # 0 = controle, 1 = t0.006, 2 = t0.0125, 3 = t0.025, 4 = t0.05 e 5 = t0.1
-data_compact <- as.data.frame(read_excel("./dados/nanoparticulas_dadosR.xlsx"))
-data_compact$Tratamento <- as.factor(data_compact$Tratamento)
-data_compact$Comprimento <- as.numeric(data_compact$Comprimento)
-data_compact$Dia <- as.factor(data_compact$Dia)
-data_compact$Repeticao <- as.factor(data_compact$Repeticao)
+data_compact_nano <- as.data.frame(read_excel("./dados/nanoparticulas_dadosR.xlsx"))
+data_compact_nano$Tratamento <- as.factor(data_compact_nano$Tratamento)
+data_compact_nano$Comprimento <- as.numeric(data_compact_nano$Comprimento)
+data_compact_nano$Dia <- as.factor(data_compact_nano$Dia)
+data_compact_nano$Repeticao <- as.factor(data_compact_nano$Repeticao)
 
-prop.table(table(is.na(data_compact))) #quantidade de NA's geral
+prop.table(table(is.na(data_compact_nano))) #quantidade de NA's geral
 
-#
-teste <- as.factor(c(rep(1:9, 60)))
-data_compact <- cbind(teste, data_compact)
+ggplot(data_compact_nano, aes(Dia, Comprimento, color = Tratamento)) + geom_boxplot(outlier.colour = "red", outlier.shape = 1)
 
-ggplot(data_compact, aes(Dia, Comprimento, color = Tratamento)) + geom_boxplot(outlier.colour = "red", outlier.shape = 1)
 
-#
+#Tentativa de fazer o perfil médio por dia
+lista1 <- list(0,0,0,0,0,0)
+lista2 <- list(0,0,0,0,0,0)
+#which(is.na(data_compact_nano$Comprimento))
+data_compact_nano$Comprimento[which(is.na(data_compact_nano$Comprimento))] <- 0
+
+for(i in 1:6){
+  lista1[[i]] <- rep(0, 9)
+  for(j in 1:9){
+   lista1[[i]][j] <- mean( as.data.frame(data_compact_nano %>% filter(Tratamento == paste0(i-1) & Dia==paste0(j-1)))$Comprimento )
+   lista2[[i]][j] <- sd( as.data.frame(data_compact_nano %>% filter(Tratamento == paste0(i-1) & Dia==paste0(j-1)))$Comprimento )
+  }
+}
+
+
+suporte <- as.data.frame(cbind(rep(0:8, 6), c(lista1[[1]], lista1[[2]], lista1[[3]], lista1[[4]], lista1[[5]], lista1[[6]]),
+                         c(lista2[[1]], lista2[[2]], lista2[[3]], lista2[[4]], lista2[[5]], lista2[[6]])))
+suporte$V1 <- as.factor(suporte$V1)
+
+
+plot(suporte$V2[1:8],axes=FALSE,ylim=c(0,0.9),cex.lab=1,xlab="Tempo",ylab="Comprimento médio")
+
+axis(2,cex.axis=1)
+
+axis(1,1:9,c("0","1","2","3","4", "5", "6", "7", "8"),cex.axis=1)
+
+
+
+plotCI(suporte$V2[1:9],liw=1.96*suporte$V3[1:9]/sqrt(10),uiw=1.96*suporte$V3[1:9]/sqrt(10),pch=19,add=TRUE,cex.lab=1,slty=1,lwd=2, col = 1, cex=1)
+
+lines(suporte$V2[1:9],lwd=1)
+
+plotCI(suporte$V2[10:18],liw=1.96*suporte$V3[10:18]/sqrt(10),uiw=1.96*suporte$V3[10:18]/sqrt(10),pch=19,add=TRUE,cex.lab=1,slty=1,lwd=2, col = 2,cex=1)
+
+lines(suporte$V2[10:18],lwd=1, col = 2)
+
+plotCI(suporte$V2[19:27],liw=1.96*suporte$V3[19:27]/sqrt(10),uiw=1.96*suporte$V3[19:27]/sqrt(10),pch=19,add=TRUE,cex.lab=1,slty=1,lwd=2, col = 3, cex=1)
+
+lines(suporte$V2[19:27],lwd=1, col = 3)
+
+plotCI(suporte$V2[28:36],liw=1.96*suporte$V3[28:36]/sqrt(10),uiw=1.96*suporte$V3[28:36]/sqrt(10),pch=19,add=TRUE,cex.lab=1,slty=1,lwd=2, col = 4,cex=1)
+
+lines(suporte$V2[28:36],lwd=1, col = 4)
+
+plotCI(suporte$V2[37:45],liw=1.96*suporte$V3[37:45]/sqrt(10),uiw=1.96*suporte$V3[37:45]/sqrt(10),pch=19,add=TRUE,cex.lab=1,slty=1,lwd=2, col = 5,cex=1)
+
+lines(suporte$V2[37:45],lwd=1, col = 5)
+
+plotCI(suporte$V2[46:54],liw=1.96*suporte$V3[46:54]/sqrt(10),uiw=1.96*suporte$V3[46:54]/sqrt(10),pch=19,add=TRUE,cex.lab=1,slty=1,lwd=2, col = 6,cex=1)
+
+lines(suporte$V2[46:54],lwd=1, col = 6)
