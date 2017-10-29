a4fa605a2fdc9debd729ee878c5578d925718dd4
HugoCalegari <hugo16calegari@gmail.com>
2017-10-29 07:53:23 -0200

    Criei uma pasta chamada pdf. Lá já coloquei algumas partes escritas e os gráficos!

--- ler_dados.R
+++ ler_dados.R
@@ -47,7 +47,12 @@ data_compact_nano$Repeticao <- as.factor(data_compact_nano$Repeticao)
 prop.table(table(is.na(data_compact_nano))) #quantidade de NA's geral
 
 data_compact_nano <- na.omit(data_compact_nano)
-ggplot(data_compact_nano, aes(Dia, Comprimento, color = Tratamento)) + geom_boxplot(outlier.colour = "red", outlier.shape = 1)
+ggplot(data_compact_nano, aes(Dia, Comprimento, color = Tratamento)) + 
+  geom_boxplot(outlier.colour = "red", outlier.shape = 1) +
+  geom_point(aes(y=Comprimento, group=Tratamento), position = position_dodge(width=0.75)) +
+  smooth.spline()
+
+#+ geom_jitter()
 
 #Tentativa de fazer o perfil médio por dia
 lista1 <- list(0,0,0,0,0,0)
@@ -60,8 +65,7 @@ for(i in 1:6){
 }
 
 
-suporte <- as.data.frame(cbind(rep(0:8, 6), c(lista1[[1]], lista1[[2]], lista1[[3]], lista1[[4]], lista1[[5]], lista1[[6]]),
-                         c(lista2[[1]], lista2[[2]], lista2[[3]], lista2[[4]], lista2[[5]], lista2[[6]])))
+suporte <- as.data.frame(cbind(rep(0:8, 6), c(lista1[[1]], lista1[[2]], lista1[[3]], lista1[[4]], lista1[[5]], lista1[[6]])))
 suporte$V1 <- as.factor(suporte$V1)
 
 
@@ -164,9 +168,25 @@ qqline(teste2, col = 2, lwd=2)
 
 #############################################################################################
 library(geepack)
-fit3 <- geeglm(Comprimento ~ Tratamento*Dia, id = Repeticao, data = data_compact_nano)
+library(plotly)
+fit <- geeglm(Comprimento ~ Tratamento*Dia, id = Repeticao, data = data_compact_nano)
 
 fit3 <- geeglm(Comprimento ~ Tratamento*Dia, family = Gamma(), id = Repeticao, data = data_compact_nano)
+
+Indice <- c(rep(0, length(fit3$fitted.values)), rep(1, length(fit3$fitted.values)))
+sup <- c(data_compact_nano$Comprimento, fit3$fitted.values)
+res_fit_true <- as.data.frame(cbind(Indice, sup))
+res_fit_true$Indice <- as.factor(res_fit_true$Indice)
+
+# 0 = valores verdadeiros de comprimento e 1 = valores ajustados de comprimento
+ggplot(res_fit_true, aes(x = Indice, y = sup, fill = Indice)) + geom_boxplot() + theme_classic() + 
+  scale_y_continuous(name = "Valores dos comprimentos") + scale_x_discrete(name = "Indices") + 
+  ggtitle("Boxplot dos comprimentos observados e ajustados") + scale_color_discrete(name = "Índice") +
+  scale_fill_discrete(name="Indice", breaks=c("0", "1"), labels=c("0 = Observado", "1 = Ajustado"))
+
+envelgama(fit.model = fit3, ligacao = "", 3)
+
+
 #############################################################################################
 
 #Com a retirada dos NA's tem dados balanceados ou não ? Observar tabela abaixo. A parir dela observa-se que
@@ -221,3 +241,35 @@ qqp(num_sum_ovo$Num_neo_somados, "pois", Ps_neo$estimate)
 
 BN_neo <- fitdistr(num_sum_ovo$Num_neo_somados, "Negative Binomial")
 qqp(num_sum_ovo$Num_neo_somados, "nbinom", size = BN_neo$estimate[[1]], mu = BN_neo$estimate[[2]])
+##########################################################
+
+plot(suporte$V2[1:9],axes=FALSE,ylim=c(0,0.9),cex.lab=1,xlab="Dia",ylab="Comprimento médio", main = "Gráfico de perfil médio do comprimento", cex = 0.5)
+
+axis(2,cex.axis=0.7)
+#c("1","2","3","4", "5", "6", "7", "8", "9")
+axis(1,1:9,c("0","1","2","3","4", "5", "6", "7", "8"),cex.axis=0.7)
+
+
+plotCI(suporte$V2[1:9],liw=0,uiw=0,pch=19,add=TRUE,cex.lab=1,lwd=1, col = 1, cex=0.5)
+
+lines(suporte$V2[1:9],lwd=2, lty = 1)
+
+plotCI(suporte$V2[10:18],liw=0,uiw=0,pch=2,add=TRUE,cex.lab=1,slty=1,lwd=2, col = 2,cex=0.5)
+
+lines(suporte$V2[10:18],lwd=1, col = 2, lty = 5)
+
+plotCI(suporte$V2[19:27],liw=0,uiw=0,pch=3,add=TRUE,cex.lab=1,slty=1,lwd=2, col = 12, cex=0.5)
+
+lines(suporte$V2[19:27],lwd=1, col = 12, lty = 4)
+
+plotCI(suporte$V2[28:36],liw=0,uiw=0,pch=4,add=TRUE,cex.lab=1,slty=1,lwd=2, col = 8,cex=0.5)
+
+lines(suporte$V2[28:36],lwd=1, col = 8, lty = 2)
+
+plotCI(suporte$V2[37:45],liw=0,uiw=0,pch=19,add=TRUE,cex.lab=1,slty=1,lwd=2, col = 5,cex=0.5)
+
+lines(suporte$V2[37:45],lwd=1, col = 5, lty = 3)
+
+plotCI(suporte$V2[46:54],liw=0,uiw=0,pch=19,add=TRUE,cex.lab=1,slty=1,lwd=2, col = 6,cex=0.5)
+
+lines(suporte$V2[46:54],lwd=1, col = 6, lty = 8)
